523c5c48c53cc843b78d1117afffa80fba64866b
Gabor Benko <benkogabor@msn.com>
2020-10-15 10:13:27 +0200

    Repository logic

---
+++ UserHandler.Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace UserHandler.Repository
+{
+    public interface IRepository<TEntitiy>  where TEntitiy : class 
+    {
+        ICollection<TEntitiy> GetAll();
+
+        TEntitiy GetById(int id);
+
+        ICollection<TEntitiy> GetBy(Expression<Func<TEntitiy, bool>> filter);
+
+        TEntitiy Create(TEntitiy entitiy);
+
+        ReplaceOneResult Update(TEntitiy entitiy);
+
+        DeleteResult Remove(TEntitiy entiy);
+
+        DeleteResult Remove(int id);
+    }
+}
---
+++ UserHandler.Repository/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UserHandler.Db;
+using UserHandler.Db.Models;
+using UserHandler.Db.Settings;
+
+namespace UserHandler.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly Lazy<IMongoCollection<User>> userCollection;
+
+        public UserRepository(IDbSettings dbSettings, IDbFactory dbFactory)
+        {
+           
+            userCollection = new Lazy<IMongoCollection<User>>(() =>
+            {
+                IMongoDatabase database = dbFactory.GetDatabase();
+                return database.GetCollection<User>(dbSettings.CollectionName);
+            }, true);
+        }
+
+        public User Create(User entitiy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<User> GetAll()
+        {
+            return userCollection.Value.Find(user=> true).ToList();
+        }
+
+        public ICollection<User> GetBy(Expression<Func<User, bool>> filter)
+        {
+            return userCollection.Value.Find(filter).ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return userCollection.Value.Find(user => user.Id == id).SingleOrDefault();
+        }
+
+        public DeleteResult Remove(User entiy)
+        {
+           return userCollection.Value.DeleteOne(user => user.Id == entiy.Id);
+        }
+
+        public DeleteResult Remove(int id)
+        {
+            return userCollection.Value.DeleteOne(user=> user.Id == id);
+        }
+
+        public ReplaceOneResult Update(User entitiy)
+        {
+            return userCollection.Value.ReplaceOne(user => user.Id == entitiy.Id, entitiy);
+        }
+    }
+}
